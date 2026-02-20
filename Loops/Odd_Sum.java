3d8c9fd7ebf76487c202d003f258d946a146b8db
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-02-20 23:36:29 +0530

    Pushing Odd Numbers Sum into git.

---
+++ Loops/Odd_Sum.java
@@ -0,0 +1,16 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Odd_Sum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter numer range");
+        int n=sc.nextInt();
+        int sum=n*n;
+        // for(int i=1;i<=n;i++){
+        //     sum+=(2*i-1);//O(n)-- is the time complexity
+        // }
+        System.out.println("the sum of first n odd numbers is: "+sum);
+    }
+}
