e0d79571bfa422791c8d8260555409fca4feb7cb
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-02-24 20:40:36 +0530

    This Program will help us tofind Nth Fibonacci Series in series

---
+++ Loops/Fibonacci_Nterms.java
@@ -0,0 +1,25 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Fibonacci_Nterms {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n=sc.nextInt();
+        
+        if(n==0){
+            System.out.println(0);
+            return;
+        }
+        int a=0,b=1;
+        for(int i=2;i<=n;i++){
+            int temp=a+b;
+            a=b;
+            b=temp;
+        }
+        System.out.println("Nth Fibonacci number is: " + b);
+
+    
+    }
+}
