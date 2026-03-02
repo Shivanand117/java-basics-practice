5b687d99f6cc37f2f3f7799a10d5123fa83079b9
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-02 23:49:08 +0530

    This program teaches how to find second largest element in the array

---
+++ Arrays/Second_Largest.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Second_Largest {
+    public static void main(String[] args) {
+        int []arr={10,10,8};
+
+        int first =Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>first){
+                second=first;
+                first=arr[i];
+            }
+              else if(arr[i] > second && arr[i] != first) {
+                second = arr[i];
+            }
+        }
+        System.out.println("Second Largest elemnt is: "+second);
+    }
+}
