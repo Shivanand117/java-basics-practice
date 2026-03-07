af7dde04ddb2022e69a55cc2adba487166de1b12
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-08 00:04:59 +0530

    This program will jelp  us to move all zeros into end

---
+++ Arrays/MoveZeroEnd.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class MoveZeroEnd {
+    public static void main(String[] args) {
+        int []arr={1,0,0,2,3,4};
+        int j=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+        for(int element:arr){
+            System.out.print(element+" ");
+        }
+
+    }
+}
