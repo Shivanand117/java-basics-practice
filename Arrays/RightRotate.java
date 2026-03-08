7a8afd33aa70991a4eaf66394f904252319402d4
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-09 00:02:31 +0530

    This prgrm help us to rotate rifght as k elemts positions

---
+++ Arrays/RightRotate.java
@@ -0,0 +1,24 @@
+package Arrays;
+public class RightRotate {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,3,4,5};
+        int k = 2;
+
+        for(int j = 0; j < k; j++) {
+
+            int last = arr[arr.length - 1];
+
+            for(int i = arr.length - 1; i > 0; i--) {
+                arr[i] = arr[i-1];
+            }
+
+            arr[0] = last;
+        }
+
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
