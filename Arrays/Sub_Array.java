56d5961e6bc813f9f3324d8a1613bc55fde6066e
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-10 12:58:48 +0530

    The program finds the subarray of a given sum value means target value and it wil return the index of values

---
+++ Arrays/Sub_Array.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Sub_Array {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,7,5};
+        int target=12;
+
+        for(int i=0;i<arr.length;i++){
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+
+            sum+=arr[j];
+            if(sum==target){
+                    System.out.println("Subarray found from index " + i + " to " + j);
+                }
+            }
+        }
+    }
+}
