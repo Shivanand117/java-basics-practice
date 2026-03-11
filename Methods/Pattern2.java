ec8fd2a94763dd056627c2e1a057e0dc7c7081f8
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-11 23:00:04 +0530

    This Program helps to print pattern to understand logic

---
+++ Methods/Pattern2.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class Pattern2 {
+    static void pattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern(10);
+    }
+}
