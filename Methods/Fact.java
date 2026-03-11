1563076487016ccb61079c9e2fed517a6982ca64
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-11 22:00:52 +0530

    This program helps us to find facorial of number

---
+++ Methods/Fact.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class Fact {
+    static int factorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }else{
+        return n*factorial(n-1);
+    }
+}
+    public static void main(String[] args) {
+        int x=4;
+        System.out.println("The factorial of number is:"+factorial(x));
+    }
+}
