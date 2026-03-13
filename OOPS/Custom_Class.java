84677ae0f460eb2d18da8a6a9de01eb9445be0b6
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-13 22:34:47 +0530

    This Prgrm helps me to understand the basic of classes and objects and how to call objects and all that

---
+++ OOPS/Custom_Class.java
@@ -0,0 +1,35 @@
+package OOPS;
+
+class Employee{
+    int id;
+    String name;
+    int salary;
+
+public void printdetails(){
+    System.out.println("My ID is:"+id);
+    System.out.println("My Name is:"+name);
+}
+public int getsalary(){
+    return salary;
+}
+}
+public class Custom_Class {
+    public static void main(String[] args) {
+        System.out.println("This is our custom class");
+        // Employee e=new Employee();
+        // e.id=17;
+        // e.name="Harry";
+        // e.salary=12000;
+        // e.printdetails();
+
+        Employee r=new Employee();
+        r.id=18;
+        r.name="Virat";
+        r.salary=15000;
+        r.printdetails();
+        int salary=r.getsalary();
+        System.out.println(salary);
+
+        
+    }
+}
