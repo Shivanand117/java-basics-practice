20dad0dd4f1fee290344dcdbbabf4bdfac4e1e9c
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-14 00:08:08 +0530

    This Prgrm helps to find area of a circle

---
+++ OOPS/Circ.java
@@ -0,0 +1,24 @@
+package OOPS;
+import java.util.*;
+class Circle{
+    double r;
+    public double  area(){
+        System.out.println("Area of circle is: ");
+        return Math.PI*r*r;
+
+    }
+}
+
+public class Circ {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Circle c=new Circle();
+        System.out.println("Enter Diameter of Circle: ");
+        double d=sc.nextDouble();
+        c.r=d/2;
+        double res=c.area();
+        System.out.println(res);
+
+        
+    }
+}
