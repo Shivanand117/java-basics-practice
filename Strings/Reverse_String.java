a4c1532413f037a485b76a7e48310105a6832a63
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-17 23:00:33 +0530

    This Program help me to understand how to reverse just using onr  for loop and also using charAt methos how to take character

---
+++ Strings/Reverse_String.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class Reverse_String {
+    public static void main(String[] args) {
+        String s="Shivanand";
+        String rev=" ";
+        for(int i=s.length()-1;i>=0;i--){
+            rev=rev+s.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
