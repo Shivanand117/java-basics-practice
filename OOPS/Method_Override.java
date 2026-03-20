55593949fa922144406b24a01b27dd978f87e7c2
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-20 22:57:39 +0530

    This Program helps me to understand the concept of method overriding and how it works by using override annonnation

---
+++ OOPS/Method_Override.java
@@ -0,0 +1,30 @@
+package OOPS;
+
+class A{
+    int a;
+    public int returns(){
+        return 4;
+    }
+    public void method2(){
+        System.out.println("I am method 2 of class A");
+    }
+}
+class B extends A{
+    @Override
+    public void method2(){
+        System.out.println("I am method 2 of class B");
+    }
+     public void method3(){
+        System.out.println("I am method 3 of class B");
+    }
+}
+
+public class Method_Override {
+   public static void main(String[] args) {
+    
+    A a=new A();
+    a.method2();
+    B b=new B();
+    b.method2();
+   } 
+}
