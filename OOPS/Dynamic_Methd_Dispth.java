9bb7481f744c01375b7703c766e32dcc44d8fa09
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-20 23:44:54 +0530

    This Program  helps me to understanfd the dynamic method dispatch how to use parents class and sub class to call overloaded methods and also leanr how can we use  objects and references to call methods present in suoer and subclass

---
+++ OOPS/Dynamic_Methd_Dispth.java
@@ -0,0 +1,37 @@
+package OOPS;
+
+class Phone{
+    public void showtime(){
+        System.out.println("Time is 11 am");
+    }
+    public void on(){
+        System.out.println("Turning on Phone..");
+
+    }
+}
+class Smartphone extends Phone{
+    public void music(){  
+        System.out.println("Playing the Music"); 
+    }
+
+    public void on(){
+        System.out.println("Turning on smaart Phone..");
+    }
+}
+
+public class Dynamic_Methd_Dispth {
+    public static void main(String[] args) {
+        // Phone p=new Phone();Allowed
+         //Phone p =new SmartPhone();//Allowed
+         //SmartPhone s=new SmartPhone();//Allowed
+        // p.greet();
+        // p.on();
+        Phone p=new Smartphone(); //yes it is allowed
+      //  p.music();
+        p.on();
+        p.showtime();
+
+
+        
+    }
+}
