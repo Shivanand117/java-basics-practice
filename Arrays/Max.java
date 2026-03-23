94f4d5f21a020f227c649ecdb672b04616431474
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-03-23 22:42:34 +0530

    This prgraamhas some xhanges by using for loop

--- Arrays/Max.java
+++ Arrays/Max.java
@@ -2,15 +2,23 @@ package Arrays;
 
 public class Max {
     public static void main(String[] args) {
-        int []a={1,45,87,98,45};
-        int max=0;
-        for ( int elemnt : a) {
-            if(elemnt>max){
-                max=elemnt;
+        int []a={1,45,87,98,100};
+        int max=a[0];
+
+        for(int i=0;i<=a.length-1;i++){
+            if(a[i]>max){
+                max=a[i];
             }
         }
-        System.out.println("Maximum aarray element is: "+max);
-    }
-}
+        System.out.println(max);
+
+//         for ( int elemnt : a) {
+//             if(elemnt>max){
+//                 max=elemnt;
+//             }
+//         }
+//         System.out.println("Maximum aarray element is: "+max);
+  }
+ }
 
 
