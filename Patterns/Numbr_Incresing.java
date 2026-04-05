3f9e924b03053a948a163dceb90b3923ac42b393
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-04-05 23:05:54 +0530

    This Program prints num ber in right angle with printing sam enumber

--- Patterns/Numbr_Incresing.java
+++ Patterns/Numbr_Incresing.java
@@ -4,8 +4,8 @@ public class Numbr_Incresing {
     public static void main(String[] args) {
         int n=5;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<i+1;j++){
-                System.out.print(j);
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
             }
             System.out.println();
         }
