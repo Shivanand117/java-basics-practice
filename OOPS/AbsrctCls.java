979fa82f0f8da68fe319a727ff7a795ea82de76f
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-05-13 10:09:56 +0530

    This Program tells us the usage of abstraction

---
+++ OOPS/AbsrctCls.java
@@ -0,0 +1,35 @@
+package OOPS;
+
+abstract class Animmal{
+	Animmal(){
+		System.out.println("We all are talking about the Animals");
+	}
+	abstract public void eat();
+}
+class Cat extends Animmal{
+	Cat(){
+		super();
+	}
+	public void eat() {
+		System.out.println("Cat is drinking milk secretely");
+	}
+}
+class Lion extends Animmal{
+	Lion(){
+		super();
+	}
+	public void eat() {
+		System.out.println("Lion is eatiing the meat of hen");
+	}
+}
+public class AbsrctCls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat c=new Cat();
+		Lion l=new Lion();
+		c.eat();l.eat();
+
+	}
+
+}
