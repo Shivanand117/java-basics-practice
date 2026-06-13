a9cfd29e9d03bfdc87e750b4fc52b580e90f652c
Shivanand Avaradi <shivanandavaradi243@gmail.com>
2026-06-13 10:28:44 +0530

    This Progrm removes adjusnt dupliate values

---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int arr[]={1, 1, 2, 2, 2, 3, 4, 4, 5};
+        int j=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]!=arr[j]){
+                j++;
+                arr[j]=arr[i];
+            }
+        }
+          System.out.println("After Removing adjusant Duplictes: ");
+        for(int i=0;i<=j;i++){
+          
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+}
