7a323822ce09f46f22c0e13e263ea642abad19e9
Shivanand <shivanandavaradi243@gmail.com>
2026-09-21 23:44:06 +0530

    Commiting

---
+++ Collectons/ThirdLargest.java
@@ -0,0 +1,32 @@
+package  Collectons;
+import java.util.Scanner;
+
+public class ThirdLargest {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("3rd Largest = " + arr[2]);
+    }
+}
